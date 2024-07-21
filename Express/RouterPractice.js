2afb5ae68e7cd6b20fe71c3291a8c5f54b1c8410
Adnan Tahir <adnantabda@gmail.com>
2024-07-21 11:12:49 +0300

    Add Router example

---
+++ Express/RouterPractice.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+const {Router} = require('express')
+
+const authorRouter = Router()
+
+authorRouter.get("/", (req, res) => res.send("All authors"));
+
+authorRouter.get("/:authorId", (req, res) => {
+  const { authorId } = req.params;
+  res.send(`Author ID: ${authorId}`);
+});
+
+app.use('/authors', authorRouter)
+app.use('/', (req, res)=>{
+    res.send("Home Page")
+})
+
+app.listen(5000)
+
